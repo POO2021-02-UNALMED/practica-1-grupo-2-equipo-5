4450692077369af8585b2db8d54aee2cd876becd
Jaime Andrés Monsalve Ballesteros <70330970+andresmonsalve19@users.noreply.github.com>
2021-12-11 22:47:40 -0500

    quité objetos de prueba

--- src/uiMain/mainUser.java
+++ src/uiMain/mainUser.java
@@ -16,10 +16,6 @@ public class mainUser {
         Scanner input = new Scanner(System.in);
         int opcion;
 
-        Cliente cl1 = new Cliente("1001025017", "jaime", "cra. 65f #30c-21", "3007242377", 125.6,"19/01/2000");
-        Cliente cl2 = new Cliente("1001025017", "andres", "cra. 65f #30c-21", "3007242377", 125.6,"19/01/2000");
-        Cajero cajero1 = new Cajero("jose", "1000", 144.5, "911", "jmonsalveb", 5000);
-        Tecnico tec1 = new Tecnico("jairo", "1001", 200, "123", "jaimemonsalveba", 12, 4);
         do {
             System.out.println("Bienvenido al sistema de PJ Tech");
             System.out.println("¿Que deseas hacer?");
